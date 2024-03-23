2a5af41654b000b1ceb8668a6ba7b66835d7d1e8
lisandro avila <lisandroam35@gmai.com>
2024-03-23 11:51:13 -0400

    first commit

---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { defineProps } from 'vue';
+
+    const props = defineProps({
+        character: []
+    })
+    console.log()
+</script>
+
+<template>
+    <div>
+        <div>
+            <img src=""alt="">
+        </div>
+        <div>
+            <h3>nombre</h3>
+        </div>
+    </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue';
+import Card from "../components/Card.vue/"
+let character = ref([])
+
+async function loadCharacters(){
+  const response = await fetch(`http://rickandmortyapi.com/api/character?page=1`);
+  const data = await response.json()
+  character.value = data.results
+  console.log(data)
+}
+loadCharacters()
+
+</script>
+
+<template>
+  <div class="flex justify-center mt-10">
+    <h1 class="text-5xl">Personajes de Rick and Morty</h1>
+  </div>
+
+  <div v-for="Personajes in character" :key="character.id">
+    {{ personajes.name }}
+
+  </div>
+
+<Card :character="character.value"></Card>
+
+</template>
